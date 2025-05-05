f999da84cd83c22a1f1f4cc77cef84f7186002e9
Troy Vassallo <dynamicconcrete@hotmail.com>
2025-05-05 19:51:07 +1000

    Styled dashboard UI for internal CRM

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
-import DashboardShell from "./components/DashboardShell";
+// app/page.js
+import Link from 'next/link';
+import DashboardShell from './components/DashboardShell';
 
 export default function Home() {
-  return <DashboardShell />;
+  return (
+    <>
+      <DashboardShell />
+      <div className="flex justify-center mt-8">
+        <Link
+          href="/second"
+          className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
+        >
+          Go to Second Page
+        </Link>
+      </div>
+    </>
+  );
 }
