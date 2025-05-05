5197c983d9251e69b10c851b6a157fb04efb0e67
Troy Vassallo <dynamicconcrete@hotmail.com>
2025-05-05 21:10:35 +1000

    Add back button to second page

---
+++ app/Data/features.js
@@ -0,0 +1,17 @@
+const features = [
+  { name: 'Projects', href: '/second', icon: 'https://img.icons8.com/ios-filled/50/construction.png' },
+  { name: 'Clients', href: '/clients', icon: 'https://img.icons8.com/ios-filled/50/user.png' },
+  { name: 'Finances', href: '/finances', icon: 'https://img.icons8.com/ios-filled/50/money.png' },
+  { name: 'Schedule', href: '/schedule', icon: 'https://img.icons8.com/ios-filled/50/planner.png' },
+  { name: 'Tenders', href: '/tenders', icon: 'https://img.icons8.com/ios-filled/50/document.png' },
+  { name: 'Reports', href: '/reports', icon: 'https://img.icons8.com/ios-filled/50/combo-chart.png' },
+  { name: 'Settings', href: '/settings', icon: 'https://img.icons8.com/ios-filled/50/settings.png' },
+  { name: 'Tasks', href: '/tasks', icon: 'https://img.icons8.com/ios-filled/50/checked-checkbox.png' },
+  { name: 'Team', href: '/team', icon: 'https://img.icons8.com/ios-filled/50/group.png' },
+  { name: 'Messages', href: '/messages', icon: 'https://img.icons8.com/ios-filled/50/speech-bubble.png' },
+  { name: 'Docs', href: '/docs', icon: 'https://img.icons8.com/ios-filled/50/documents.png' },
+  { name: 'Help', href: '/help', icon: 'https://img.icons8.com/ios-filled/50/help.png' },
+];
+
+export default features;
+
